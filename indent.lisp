640b17740dd7ae45bc86d41f24000d9fbbaf5555
Noodles! <nnoodle@chiru.no>
2021-05-04 23:23:06 +0700

    Update indentation on setf
    At least sly needs slynk:update-indentation-information called in order for changes to take effect in the editor.

--- indent.lisp
+++ indent.lisp
@@ -18,13 +18,29 @@
 
 (defvar *indentation-hints* (make-hash-table :test #'eq))
 
+(defmacro with-symbol (name-symbol-package-module &body body)
+  "Check if SYMBOL exists in PACKAGE when MODULE is loaded and bind it to NAME."
+  (destructuring-bind (name symbol package module)
+      name-symbol-package-module
+    (let ((pkg (gensym)))
+      `(when (member ,module *modules* :test #'string=)
+         (let* ((,pkg (find-package ,package))
+                (,name (when ,pkg (find-symbol (string ,symbol) ,pkg))))
+           (when ,name
+             ,@body))))))
+
 (defun indentation (symbol)
   "Returns the custom defined indentation of a symbol if there is any. SETF-able."
   (gethash symbol *indentation-hints*))
 
 (defun (setf indentation) (rule-form symbol)
   "Sets the indentation hint for a symbol."
-  (setf (gethash symbol *indentation-hints*) rule-form))
+  (setf (gethash symbol *indentation-hints*) rule-form)
+  (with-symbol (update '#:update-indentation-information :swank "SWANK-INDENTATION")
+    (funcall (symbol-function update)))
+  (with-symbol (update '#:update-indentation-information :slynk "SLYNK/INDENTATION")
+    (funcall (symbol-function update)))
+  rule-form)
 
 (defmacro define-indentation (symbol rule-form)
   "Define an indentation hint for a symbol.
@@ -46,12 +62,9 @@ It should be safe to call this function regardless of whether
 SWANK is loaded at all or not.
 
 This is automatically called when TRIVIAL-INDENT is loaded."
-  (when (member "SWANK-INDENTATION" *modules* :test #'string=)
-    (let* ((swank (find-package :swank))
-           (tables (when swank (find-symbol (string '#:*application-hints-tables*) swank))))
-      (when tables
-        (set tables (cons *indentation-hints* (remove *indentation-hints* (symbol-value tables))))
-        t))))
+  (with-symbol (tables '#:*application-hints-tables* :swank "SWANK-INDENTATION")
+    (set tables (cons *indentation-hints* (remove *indentation-hints* (symbol-value tables))))
+    t))
 
 (defun initialize-sly ()
   "Attempts to initialize sly with our indentation table.
@@ -60,12 +73,9 @@ It should be safe to call this function regardless of whether
 SLYNK is loaded at all or not.
 
 This is automatically called when TRIVIAL-INDENT is loaded."
-  (when (member "SLYNK/INDENTATION" *modules* :test #'string=)
-    (let* ((slynk (find-package :slynk))
-           (tables (when slynk (find-symbol (string '#:*application-hints-tables*) slynk))))
-      (when tables
-        (set tables (cons *indentation-hints* (remove *indentation-hints* (symbol-value tables))))
-        t))))
+  (with-symbol (tables '#:*application-hints-tables* :slynk "SLYNK/INDENTATION")
+    (set tables (cons *indentation-hints* (remove *indentation-hints* (symbol-value tables))))
+    t))
 
 (initialize-slime)
 (initialize-sly)
